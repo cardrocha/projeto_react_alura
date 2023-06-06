1b1b6758825c04423a38e81c2cf72dd46d9c2010
Ricardo Rocha <cardrocharj@gmail.com>
2023-06-06 18:51:30 -0300

    fixFooter

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -5,8 +5,8 @@ export const ContainerFooter = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  background-color: ${colors.background};
-  border-top: 2px solid ${colors.border};
+  background-color: ${colors.black};
+  border-top: 2px solid ${colors.navy_blue};
 
   img {
     padding: 20px 0 60px 0;
